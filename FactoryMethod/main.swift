8e9f9600a23dc04b928f5ad6a0f6ab51f944e9ba
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-11-11 15:14:50 -0500

    Initial Commit

---
+++ FactoryMethod/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FactoryMethod
+//
+//  Created by Tim Pryor on 2015-11-11.
+//  Copyright (c) 2015 Tim Pryor. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
